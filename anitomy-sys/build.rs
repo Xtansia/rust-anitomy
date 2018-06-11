4a14f2e24a55ce53cc416cb5716d2a6147ad3a1f
Thomas Farr <xtansia@xtansia.com>
2018-06-12 01:54:32 +1200

    Use cc directly instead of cmake

--- anitomy-sys/build.rs
+++ anitomy-sys/build.rs
@@ -1,8 +1,21 @@
-extern crate cmake;
+extern crate cc;
 
 fn main() {
-    let dst = cmake::build("anitomy-c");
+    if cfg!(not(target_env = "msvc")) {
+        std::env::set_var("CXXFLAGS", "-std=c++14");
+    }
 
-    println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=static=lib/anitomy_c");
+    cc::Build::new()
+        .cpp(true)
+        .file("anitomy-c/anitomy_c.cpp")
+        .file("anitomy-c/anitomy/anitomy/anitomy.cpp")
+        .file("anitomy-c/anitomy/anitomy/element.cpp")
+        .file("anitomy-c/anitomy/anitomy/keyword.cpp")
+        .file("anitomy-c/anitomy/anitomy/parser.cpp")
+        .file("anitomy-c/anitomy/anitomy/parser_helper.cpp")
+        .file("anitomy-c/anitomy/anitomy/parser_number.cpp")
+        .file("anitomy-c/anitomy/anitomy/string.cpp")
+        .file("anitomy-c/anitomy/anitomy/token.cpp")
+        .file("anitomy-c/anitomy/anitomy/tokenizer.cpp")
+        .compile("anitomy_c");
 }
